1aeb5c6f1fe06ffc0d4d69e57fe19845888c4099
HADI <155739994+HQ-Coding@users.noreply.github.com>
2024-10-29 23:49:18 +0330

    note #02

--- notePad/js/script.js
+++ notePad/js/script.js
@@ -1,1 +1,23 @@
- 
+let color = document.getElementById('color');
+let createBtn = document.getElementById('createBtn');
+let list = document.getElementById('list');
+
+createBtn.onclick = () => {
+  let newNote = document.createElement('div');
+  newNote.classList.add('note');
+  newNote.innerHTML = `
+    <span class="close">X</span>
+    <textarea placeholder="Write Content..." rows="10" cols="30"></textarea>`;
+
+  newNote.style.borderColor = color.value;
+  list.appendChild(newNote);
+};
+
+
+document.addEventListener('click' ,(event)=>{
+    console.log(event.target  );
+    console.log(event.target.parentNode);
+    if(event.target.classList.contains('close')){
+        event.target.parentNode.remove() 
+    }
+})
